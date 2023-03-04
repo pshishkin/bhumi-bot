e204780b4db24d596097204f1619245560a98eca
Rustam Babadjanov <r.babadjanov@algori.io>
2023-03-04 11:28:29 +0200

    Added mapping buttons and notification

--- src/photo_controller.py
+++ src/photo_controller.py
@@ -12,6 +12,9 @@ class PhotoController(BaseMongoRepository):
 
     async def add_photo(self, user_id: int, photo_id: str) -> ObjectId:
         obj_id = ObjectId()
-        await self._insert_one({'_id': obj_id, 'photo_id': photo_id})
+        await self._insert_one({'_id': obj_id, 'photo_id': photo_id, 'user_id': user_id})
         return obj_id
 
+    async def get_photo_sender(self, obj_id: str) -> int:
+        photo = await self._find_one({'_id': ObjectId(obj_id)})
+        return photo['user_id']
