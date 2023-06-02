ed45072ec96d066048bcb94ab0c8e9577b6d5ccb
pavel <pshishkin@gmail.com>
2023-06-02 15:55:00 +0300

    36 for S and removed Kuryshev

--- src/web.py
+++ src/web.py
@@ -233,7 +233,7 @@ def get_user_allowance(user_id: str) -> (int, str):
     if found_in_groups:
         details = ', '.join(found_in_groups)
         allowance_user += settings.BHUMI_DROP_BASE * len(found_in_groups)
-        if "\u0428\u043c\u0438\u044216" in found_in_groups:
+        if "\u0428\u043c\u0438\u044216" in found_in_groups or "Syntony" in found_in_groups:
             allowance_user += settings.BHUMI_DROP_BASE * 2
 
     return allowance_user, details
