45f655d0a2f1e3d57a9aff7b189695e280519837
pavel <pshishkin@gmail.com>
2023-06-05 22:15:47 +0300

    distribution of bhumis done

--- src/crypto_test.py
+++ src/crypto_test.py
@@ -17,7 +17,7 @@ async def main():
 
     wallets = await wallet_controller.get_all_wallets()
 
-    for w in wallets[:3]:
+    for w in wallets[2:3]:
         res = await crypto.transfer_all_with_ratios(
             w.keypair,
             crypto.daily_stash_keypair,
